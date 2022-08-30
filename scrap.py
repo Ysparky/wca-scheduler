23681364cc022cd3f0213b983b1d175017628298
Jose Caldas <jos.caldas.27@gmail.com>
2022-08-30 06:45:02 -0500

    feat: dates and comp name

--- scrap.py
+++ scrap.py
@@ -1,48 +1,56 @@
-import pandas
 from bs4 import BeautifulSoup
+from dateutil import parser
 import requests
 import pandas as pd
 
-url = 'https://www.worldcubeassociation.org/competitions/RequinoaOpen2022#competition-schedule'
+url = 'https://www.worldcubeassociation.org/competitions//RealCube2022#competition-schedule'
 page = requests.get(url, verify=False)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 words_to_ignore = ['From', 'To', 'Format', 'Time limit', 'Cutoff', 'Proceed', '*', '**']
 
-# schedule_table_container = soup.find_next('div', class_='schedule_table_container')
-# print(len(schedule_table_container))
+h3_tags = soup.find_all('h3')
 
-schedule_table = soup.find('div', class_='schedule-table')
-schedule_table_list = list()
+competition_name = h3_tags[0].text.strip()
+competition_dates = list()
 
-for item in schedule_table.children:
-    if item != "\n":
-        schedule_table_list.append(item)
+for item in h3_tags:
+    sentence = item.text.strip()
+    if "(" in sentence and ")" in sentence:
+        date = sentence[sentence.find('(')+1:sentence.find(')')]
+        competition_dates.append(parser.parse(date))
 
-row_schedule = list()
-for item in schedule_table_list:
-    stripped_item = list()
-    for line in item.text.splitlines():
-        if line.strip():
-            stripped_line = line.strip()
-            if stripped_line not in words_to_ignore:
-                stripped_item.append(stripped_line)
-    row_schedule.append(stripped_item)
-
-for item in row_schedule:
-    item_length = len(item)
-    if item_length == 7:
-        last_item = item[6]
-        first_char = last_item[0]
-        if first_char.isnumeric():
-            item.append('')
-        else:
-            item.insert(6, '')
-    else:
-        difference = 8 - item_length
-        if difference > 0:
-            for x in range(0, difference):
-                item.append('')
+# schedule_table = soup.find('div', class_='schedule-table')
+# schedule_table_list = list()
+#
+# for item in schedule_table.children:
+#     if item != "\n":
+#         schedule_table_list.append(item)
+#
+# row_schedule = list()
+# for item in schedule_table_list:
+#     stripped_item = list()
+#     for line in item.text.splitlines():
+#         if line.strip():
+#             stripped_line = line.strip()
+#             if stripped_line not in words_to_ignore:
+#                 stripped_item.append(stripped_line)
+#     row_schedule.append(stripped_item)
+#
+# for item in row_schedule:
+#     item_length = len(item)
+#     if item_length == 7:
+#         last_item = item[6]
+#         first_char = last_item[0]
+#         if first_char.isnumeric():
+#             item.append('')
+#         else:
+#             item.insert(6, '')
+#     else:
+#         difference = 8 - item_length
+#         if difference > 0:
+#             for x in range(0, difference):
+#                 item.append('')
 
 # print(row_schedule)
 
